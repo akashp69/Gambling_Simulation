442aee5883dbdce8cbead88f1617116dacc2ec07
akashp69 <akashpakhare499@gmail.com>
2022-12-12 16:15:48 +0530

    Added: Program For check gambler win or lose

--- src/com/bridgelabz/BettingGame.java
+++ src/com/bridgelabz/BettingGame.java
@@ -1,30 +1,45 @@
 package com.bridgelabz;
 
+
+import java.util.Random;
+
 public class BettingGame {
-    static int TOTAL_MONEY = 100;
-    static int totalMoney = 0;
-    static int noOfPlays = 1;
-    static int count = 0;
-    static int WINNING_AMOUNT = 0;
-    static int LOSING_AMOUNT = 0;
+    /**
+     * starting with a stake of $100 every day and bet $1 every game.
+     * Genrate a random in static
+     * make $1 bet so either win or loose $1
+     */
+    public static final  int initialStake = 100;
+    public static final int stakeBet = 1;
+    static Random random = new Random();
 
     /**
-     * This method is used for gambler stake $100 everyday and $1 on every game
+     * This is Main Method is used for calling  static and instance variables
      */
-    public void startStake() {
-        TOTAL_MONEY = 100;
-        int startGame = TOTAL_MONEY - 1;
-        System.out.println("After first Stake Remaining Money = " + startGame + "$");
+    public static void main(String[] args)
+    {
+        int totalStake = initialStake;
+
+        System.out.println("Welcome to Gambling Game Simulator program");
+        /**
+        *Genrate random Numbers in range 0,1
+         */
+        int play = random.nextInt(2);
+
+        switch(play)
+        {
+            case 0:
+                totalStake = totalStake - stakeBet;
+                System.out.println("Loose the Bet");
+                System.out.println("Stake after loosing: "+totalStake);
+                break;
+            case 1:
+                totalStake = totalStake + stakeBet;
+                System.out.println("Won the Bet");
+                System.out.println("Stake after winning: "+totalStake);
+                break;
+        }
+
     }
 
-    /**
-     *This is main method is used for calling methods
-     */
-    public static void main(String[] args) {
-        System.out.println("Gambler have 100$ to Bet");
-        System.out.println("Gambler's total Money = " + TOTAL_MONEY + "$");
-        BettingGame firstStake = new BettingGame();
-        firstStake.startStake();
-
-    }
 }
